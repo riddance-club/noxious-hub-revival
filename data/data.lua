db45adfd86ffc0c277a4a36ccba20ed05aa86072
olivia1246 <135425765+olivia1246@users.noreply.github.com>
2025-12-08 12:55:37 +0200

    Update data.lua

--- data/data.lua
+++ data/data.lua
@@ -459,7 +459,7 @@ config.changelogs = {
     "- Renamed 'Teleports' tab to 'Teleports / Travel'.",
     "- Implemented better error logging for commands.",
     "- Added (!) to some still incomplete commands.",
-}
+},
 config.information = {
     "Information:",
     " ",
